6de4254704afbd4009439cf64bae4925b5aa1119
Nimas <nimasajeng2019@gmail.com>
2023-12-18 11:56:22 +0700

    add mood (plis aku stress bgt querynya yg bener gimana)

--- Cloud Computing/src/routes/mood.js
+++ Cloud Computing/src/routes/mood.js
@@ -1,70 +1,63 @@
+// app.js
+
 const express = require('express');
-const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
+
+// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Konfigurasi koneksi ke database MySQL
+// MySQL Connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to database:', err);
+    console.error('Error connecting to MySQL:', err);
   } else {
-    console.log('Connected to database');
+    console.log('Connected to MySQL');
   }
 });
 
-// Endpoint untuk mendapatkan data Mood Tracker
-app.get('/mood-tracker', (req, res) => {
-  const { sbuser_id, date } = req.query;
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, Mood Tracker API!');
+});
+
+// GET route to retrieve and calculate mood
+app.get('/mood/:sbuser_id', (req, res) => {
+  const sbuser_id = req.params.sbuser_id;
 
   const sql = `
-    SELECT c.fullname, t.mood, t.timestamp
-    FROM community c
-    LEFT JOIN tracker t ON c.post_id = t.post_id
-    WHERE c.sbuser_id = ? AND DATE(t.timestamp) = ?
+    SELECT AVG(mood) AS average_mood
+    FROM community
+    WHERE sbuser_id = ?;
   `;
 
-  db.query(sql, [sbuser_id, date], (err, result) => {
+  db.query(sql, [sbuser_id], (err, result) => {
     if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
+      console.error('Error retrieving mood:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
     } else {
-      res.status(200).json(result);
+      const averageMood = result[0].average_mood || 0;
+      res.json({ average_mood: averageMood });
     }
   });
 });
 
-// Endpoint untuk menyimpan mood ke dalam tabel mood
-app.post('/mood', (req, res) => {
-  const { sbuser_id, post_id, message_id, fullname, mood, timestamp } = req.body;
-
-  const sql = `
-    INSERT INTO mood (sbuser_id, post_id, message_id, fullname, mood, timestamp)
-    VALUES (?, ?, ?, ?, ?, ?)
-  `;
-
-  db.query(sql, [sbuser_id, post_id, message_id, fullname, mood, timestamp], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.status(200).send('Mood saved successfully');
-    }
-  });
-});
-
-// Menjalankan server pada port yang telah ditentukan
+// Start Server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
