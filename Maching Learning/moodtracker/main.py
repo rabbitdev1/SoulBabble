35784f79388c88b2a58dcd1f43dc63d68d3eb4ed
NurmalitaFitri <113280025+NurmalitaFitri@users.noreply.github.com>
2023-12-19 01:36:16 +0700

    moodtrack-api

--- Maching Learning/moodtracker/main.py
+++ Maching Learning/moodtracker/main.py
@@ -1,22 +1,54 @@
-from fastapi import FastAPI
-
+from MySQLdb import Timestamp
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from datetime import datetime
+from dotenv import load_dotenv
+from sqlalchemy import Text, create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from model import prediksi
-from dotenv import main
 import os
 
-main.load_dotenv()
+load_dotenv()
 
+# Konfigurasi koneksi ke MySQL
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+mysqlconnector://soul:soulbabble@35.212.162.13/babble")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Definisi model tabel
+class Community(Base):
+    __tablename__ = "community"
+    post_id = Column(Integer, primary_key=True, index=True)  # Tambahkan post_id sebagai primary key
+    sbuser_id = Column(Integer, index=True)  # Tambahkan kolom user_id
+    fullname = Column(String(255))  # Tambahkan kolom fullname
+    message = Column(Text)  # Tambahkan kolom message dengan tipe Text
+    timestamp = Column(DateTime, default=datetime.utcnow)
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"msg":"Mainpage !"}
+# Dependency untuk mendapatkan sesi database
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.post("/predict")
-def predictor(st):
-    result = prediksi(st)
-    return {"msg":result}
+@app.post("/predict/{sbuser_id}")
+def predictor(sbuser_id: str, db: Session = Depends(get_db)):
+    # Mengambil pesan terbaru dari tabel untuk user_id tertentu
+    latest_message = db.query(Community).filter(Community.sbuser_id == sbuser_id).order_by(Community.timestamp.desc()).first()
+
+    if not latest_message:
+        raise HTTPException(status_code=404, detail=f"No messages found for user_id: {sbuser_id}")
+
+    # Melakukan prediksi menggunakan model
+    result = prediksi(latest_message.message)
+
+    return {"msg": result}
 
 
 import uvicorn
