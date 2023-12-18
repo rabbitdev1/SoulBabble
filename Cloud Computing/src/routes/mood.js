eeee043c9e3271e76a927fd20307f12d7506345e
Nimas <nimasajeng2019@gmail.com>
2023-12-18 20:30:15 +0700

    add mood (blm bener)

--- Cloud Computing/src/routes/mood.js
+++ Cloud Computing/src/routes/mood.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const { loadModel } = require('./your_ml_model_util'); // Sesuaikan dengan lokasi utilitas Anda untuk memuat model
 
 // Load environment variables from .env file
 dotenv.config();
@@ -36,26 +37,39 @@ app.get('/', (req, res) => {
   res.send('Hello, Mood Tracker API!');
 });
 
-// GET route to retrieve and calculate mood
+// GET route to retrieve and calculate mood using ML model
 app.get('/mood/:sbuser_id', (req, res) => {
   const sbuser_id = req.params.sbuser_id;
 
-  const sql = `
-    SELECT AVG(mood) AS average_mood
+  // Ambil data dari tabel community
+  const sqlCommunity = `
+    SELECT message
     FROM community
     WHERE sbuser_id = ?;
   `;
 
-  db.query(sql, [sbuser_id], (err, result) => {
+  db.query(sqlCommunity, [sbuser_id], (err, resultCommunity) => {
     if (err) {
-      console.error('Error retrieving mood:', err);
+      console.error('Error retrieving community posts:', err);
       res.status(500).json({ error: 'Internal Server Error' });
     } else {
-      const averageMood = result[0].average_mood || 0;
-      res.json({ average_mood: averageMood });
+      const communityPosts = resultCommunity.map((row) => row.message);
+
+      // Contoh: Logika perhitungan mood menggunakan model ML (sesuaikan dengan model Anda)
+      const moodResult = predictMoodUsingMLModel(communityPosts);
+
+      res.json({ community_posts: communityPosts, mood_result: moodResult });
     }
   });
 });
+
+// Fungsi untuk memuat dan menggunakan model ML (sesuaikan dengan model Anda)
+function predictMoodUsingMLModel(communityPosts) {
+    const mlModel = loadModel(); // Memuat model ML yang telah dilatih
+    const inputFeatures = preprocessData(communityPosts); // Sesuaikan dengan preprocessing yang diperlukan
+    const predictedMood = mlModel.predict(inputFeatures);
+    return predictedMood;
+  }
 
 // Start Server
 app.listen(port, () => {
