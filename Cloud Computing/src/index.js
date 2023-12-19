3f4a1a2651969274f4d36a6a501178130a13f5a0
Nimas <nimasajeng2019@gmail.com>
2023-12-19 22:29:00 +0700

    Add axios and update index.js with predict routes from other api

--- Cloud Computing/src/index.js
+++ Cloud Computing/src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const journalRouter = require('./routes/journal');
 const chatRoutes = require('./routes/chatbot');
-
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
@@ -13,6 +13,25 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/journal', journalRouter);
 app.use('/chatbot', chatRoutes);
+
+app.post('/predict', async (req, res) => {
+  try {
+      const response = await axios.post('https://soulbabble-py-api-v6deafcxhq-uc.a.run.app/predict', req.body);
+      res.json(response.data);
+  } catch (error) {
+      res.status(500).json({ error: 'Error forwarding request' });
+  }
+});
+
+app.post('/predict/:sbuser_id', async (req, res) => {
+  try {
+      const response = await axios.post(`https://soulbabble-py-api-v6deafcxhq-uc.a.run.app/predict/${req.params.sbuser_id}`, req.body);
+      res.json(response.data);
+  } catch (error) {
+      res.status(500).json({ error: 'Error forwarding request' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
